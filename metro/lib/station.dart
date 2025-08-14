a52225d1e84ee1b022b3c0a062f39048a180f92b
Eng Hisham Hatem <heshamhatem08@gmail.com>
2025-08-15 01:01:47 +0300

    adding new class

---
+++ metro/lib/station.dart
@@ -0,0 +1,440 @@
+class Station {
+  final String name;
+  final double latitude;
+  final double longitude;
+
+  const Station({
+    required this.name,
+    required this.latitude,
+    required this.longitude,
+  });
+}
+
+const stationsCoordinates = <Station>[
+  Station(
+    name: 'elmounib',
+    latitude: 29.981153305051627,
+    longitude: 31.2123248464122,
+  ),
+  Station(
+    name: 'sakiat mekky',
+    latitude: 29.99683851014299,
+    longitude: 31.209687039894938,
+  ),
+  Station(
+    name: 'omm elmasryeen',
+    latitude: 30.006991782739266,
+    longitude: 31.208721013595845,
+  ),
+  Station(
+    name: 'giza',
+    latitude: 30.012268810941656,
+    longitude: 31.21052345799687,
+  ),
+  Station(
+    name: 'faisal',
+    latitude: 30.018734646116656,
+    longitude: 31.206060262423964,
+  ),
+
+  Station(
+    name: 'el bohoth',
+    latitude: 30.03575693374353,
+    longitude: 31.200237729349862,
+  ),
+  Station(
+    name: 'dokki',
+    latitude: 30.038580474386347,
+    longitude: 31.212382771709272,
+  ),
+  Station(
+    name: 'opera',
+    latitude: 30.041930561068206,
+    longitude: 31.224967171069405,
+  ),
+  Station(
+    name: 'sadat',
+    latitude: 30.044135624843225,
+    longitude: 31.2343258411071,
+  ),
+  Station(
+    name: 'mohamed naguib',
+    latitude: 30.045332025767635,
+    longitude: 31.24387664281836,
+  ),
+  Station(
+    name: 'attaba',
+    latitude: 30.052354554235837,
+    longitude: 31.246746221860278,
+  ),
+  Station(
+    name: 'alshohadaa',
+    latitude: 30.06112117984301,
+    longitude: 31.24601235571627,
+  ),
+  Station(
+    name: 'masarra',
+    latitude: 30.07089750357882,
+    longitude: 31.24505162733648,
+  ),
+  Station(
+    name: 'rod elfarag',
+    latitude: 30.08060115500998,
+    longitude: 31.245324829280033,
+  ),
+  Station(
+    name: 'st. teresa',
+    latitude: 30.087954126175084,
+    longitude: 31.245403641595765,
+  ),
+  Station(
+    name: 'khalafawy',
+    latitude: 30.097880267137104,
+    longitude: 31.245357832326004,
+  ),
+  Station(
+    name: 'mezallat',
+    latitude: 30.10420987973398,
+    longitude: 31.245715145143325,
+  ),
+  Station(
+    name: 'kolleyyet elzeraa',
+    latitude: 30.1137024739906,
+    longitude: 31.24861230297706,
+  ),
+  Station(
+    name: 'شبرا',
+    latitude: 30.12243338759986,
+    longitude: 31.2444174077958,
+  ),
+  Station(
+    name: 'helwan',
+    latitude: 29.84906291272719,
+    longitude: 31.334249516350734,
+  ),
+  Station(
+    name: 'ain helwan',
+    latitude: 29.8625477028623,
+    longitude: 31.324520496176994,
+  ),
+  Station(
+    name: 'helwan university',
+    latitude: 29.869506920684504,
+    longitude: 31.319323735848567,
+  ),
+  Station(
+    name: 'wadi hof',
+    latitude: 29.87917728709114,
+    longitude: 31.31300066340233,
+  ),
+  Station(
+    name: 'hadayek helwan',
+    latitude: 29.897305314527753,
+    longitude: 31.304223586231874,
+  ),
+  Station(
+    name: 'elmaasara',
+    latitude: 29.906065221936977,
+    longitude: 31.29927197405296,
+  ),
+  Station(
+    name: 'كوتسكا',
+    latitude: 29.93627993477272,
+    longitude: 31.281684536917687,
+  ),
+  Station(
+    name: 'tora elbalad',
+    latitude: 29.946779392790393,
+    longitude: 31.27284748233696,
+  ),
+  Station(
+    name: 'sakanat elmaadi',
+    latitude: 29.952889749423992,
+    longitude: 31.263304993492834,
+  ),
+  Station(
+    name: 'elmaadi',
+    latitude: 29.960300625180196,
+    longitude: 31.257598686204915,
+  ),
+  Station(
+    name: 'hadayek elmaadi',
+    latitude: 29.97017828396375,
+    longitude: 31.25071289347021,
+  ),
+  Station(
+    name: 'dar elsalam',
+    latitude: 29.982077011104984,
+    longitude: 31.242083944660784,
+  ),
+  Station(
+    name: 'elzahraa',
+    latitude: 29.995479008649994,
+    longitude: 31.23114261084708,
+  ),
+  Station(
+    name: 'mar girgis',
+    latitude: 30.006085517113085,
+    longitude: 31.229560077045114,
+  ),
+  Station(
+    name: 'alsayeda zeinab',
+    latitude: 30.01770823102937,
+    longitude: 31.231209945452218,
+  ),
+  Station(
+    name: 'alsayeda zeinab',
+    latitude: 30.02928621683731,
+    longitude: 31.235378293669967,
+  ),
+  Station(
+    name: 'saad zaghloul',
+    latitude: 30.036995417539316,
+    longitude: 31.238291241296075,
+  ),
+  Station(
+    name: 'sadat',
+    latitude: 30.044144315827513,
+    longitude: 31.234341955551574,
+  ),
+  Station(
+    name: 'gamal abdel nasser',
+    latitude: 30.05350437670406,
+    longitude: 31.238575323162458,
+  ),
+  Station(
+    name: 'orabi',
+    latitude: 30.05668214525008,
+    longitude: 31.241925375657445,
+  ),
+  Station(
+    name: 'ghamra',
+    latitude: 30.069050803590418,
+    longitude: 31.26453922150748,
+  ),
+  Station(
+    name: 'eldemerdash',
+    latitude: 30.07732392619634,
+    longitude: 31.27775632026404,
+  ),
+  Station(
+    name: 'manshiet elsadr',
+    latitude: 30.08200520621658,
+    longitude: 31.287443316667925,
+  ),
+  Station(
+    name: 'kobri elqobba',
+    latitude: 30.08717899479954,
+    longitude: 31.294048371384804,
+  ),
+  Station(
+    name: 'hammamat elqobba',
+    latitude: 30.091237156546157,
+    longitude: 31.29878548785252,
+  ),
+  Station(
+    name: 'hadayek elzaitoun',
+    latitude: 30.10590648113526,
+    longitude: 31.310373651025348,
+  ),
+  Station(
+    name: 'helmeyet elzaitoun',
+    latitude: 30.113255547692592,
+    longitude: 31.31395870003386,
+  ),
+  Station(
+    name: 'elmatareyya',
+    latitude: 30.121345747270265,
+    longitude: 31.313651033437125,
+  ),
+  Station(
+    name: 'ain shams',
+    latitude: 30.131027269809277,
+    longitude: 31.319019008066917,
+  ),
+  Station(
+    name: 'ezbet elnakhl',
+    latitude: 30.139308236948036,
+    longitude: 31.32425628708358,
+  ),
+  Station(
+    name: 'elmarg',
+    latitude: 30.152079221498774,
+    longitude: 31.335608453250845,
+  ),
+  Station(
+    name: 'new elmarg',
+    latitude: 30.16364756817454,
+    longitude: 31.338452302596988,
+  ),
+  Station(
+    name: 'adly mansour',
+    latitude: 30.14644061768812,
+    longitude: 31.421889469843254,
+  ),
+  Station(
+    name: 'elhaykestep',
+    latitude: 30.14387464581446,
+    longitude: 31.40460311019347,
+  ),
+  Station(
+    name: 'omar ibn elkhattab',
+    latitude: 30.140344127371513,
+    longitude: 31.394119207647915,
+  ),
+  Station(
+    name: 'qubaa',
+    latitude: 30.134912082054207,
+    longitude: 31.383910135842505,
+  ),
+  Station(
+    name: 'hesham barakat',
+    latitude: 30.130841150759263,
+    longitude: 31.37262772897626,
+  ),
+  Station(
+    name: 'elnozha',
+    latitude: 30.12798890489869,
+    longitude: 31.359986955793687,
+  ),
+  Station(
+    name: 'el shams club',
+    latitude: 30.125516055352985,
+    longitude: 31.348766791024847,
+  ),
+  Station(
+    name: 'alf masken',
+    latitude: 30.119021046283745,
+    longitude: 31.340046485700974,
+  ),
+  Station(
+    name: 'heliopolis',
+    latitude: 30.108427100225203,
+    longitude: 31.33821081760593,
+  ),
+  Station(
+    name: 'haroun',
+    latitude: 30.10138032459675,
+    longitude: 31.332826339324857,
+  ),
+  Station(
+    name: 'alahram',
+    latitude: 30.091720368246854,
+    longitude: 31.326247759698028,
+  ),
+  Station(
+    name: 'koleyet elbanat',
+    latitude: 30.084017729471046,
+    longitude: 31.328917657384896,
+  ),
+  Station(
+    name: 'stadium',
+    latitude: 30.07291879752378,
+    longitude: 31.316891283229296,
+  ),
+  Station(
+    name: 'fair zone',
+    latitude: 30.073261208906548,
+    longitude: 31.300721357598643,
+  ),
+  Station(
+    name: 'abbassiya',
+    latitude: 30.071969095690793,
+    longitude: 31.283263286653845,
+  ),
+  Station(
+    name: 'abdou pasha',
+    latitude: 30.064800698465902,
+    longitude: 31.274638864611692,
+  ),
+  Station(
+    name: 'elgeish',
+    latitude: 30.0617613414427,
+    longitude: 31.266749131605618,
+  ),
+  Station(
+    name: 'bab elshaariya',
+    latitude: 30.05416371419143,
+    longitude: 31.255704923358987,
+  ),
+  Station(
+    name: 'العتبة',
+    latitude: 30.052298172850314,
+    longitude: 31.246607043447383,
+  ),
+  Station(
+    name: 'جمال عبد الناصر',
+    latitude: 30.053491802810218,
+    longitude: 31.238485012241647,
+  ),
+  Station(
+    name: 'maspero',
+    latitude: 30.055743235104515,
+    longitude: 31.231919101335,
+  ),
+  Station(
+    name: 'safaa hijazy',
+    latitude: 30.0622708921459,
+    longitude: 31.223108239702945,
+  ),
+  Station(
+    name: 'kit kat',
+    latitude: 30.066536402751584,
+    longitude: 31.212860662281102,
+  ),
+  Station(
+    name: 'sudan',
+    latitude: 30.070057091250302,
+    longitude: 31.205351372991966,
+  ),
+  Station(
+    name: 'imbaba',
+    latitude: 30.075749997845495,
+    longitude: 31.206430911903492,
+  ),
+  Station(
+    name: 'elbohy',
+    latitude: 30.08208479612511,
+    longitude: 31.209433379578986,
+  ),
+  Station(
+    name: 'elqawmia',
+    latitude: 30.093251431777208,
+    longitude: 31.20888640637408,
+  ),
+  Station(
+    name: 'ring road',
+    latitude: 30.096395740887445,
+    longitude: 31.199690291547196,
+  ),
+  Station(
+    name: 'rod elfarag corridor',
+    latitude: 30.10192054628204,
+    longitude: 31.184335967902875,
+  ),
+  Station(
+    name: 'tawfikia',
+    latitude: 30.065070451203624,
+    longitude: 31.203106542770044,
+  ),
+  Station(
+    name: 'wadi el nile',
+    latitude: 30.05696041333326,
+    longitude: 31.20093050585198,
+  ),
+  Station(
+    name: 'gamet el dowal',
+    latitude: 30.050092221988287,
+    longitude: 31.198824651863923,
+  ),
+  Station(
+    name: 'boulak el dakrour',
+    latitude: 30.037556775795778,
+    longitude: 31.19604873311913,
+  ),
+  Station(
+    name: 'cairo university',
+    latitude: 30.025106041571,
+    longitude: 31.201331739593027,
+  ),
+];
